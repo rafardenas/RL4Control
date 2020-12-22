a2f21d2f870af56bc8476be8ba7b86dbe52d9d69
Rafael Cardenas <rafardenas@gmail.com>
2020-12-22 15:55:49 -0600

    Added MC control

--- gym/Agents/Q_Learning.py
+++ gym/Agents/Q_Learning.py
@@ -22,7 +22,7 @@ class Q_agent():
         self.states = states
         self.qsa_pairs = qsa_pairs
         self.policy = policy
-
+        self.total_reward = 0
 
     def state_discretizer(self, s, v): #going to be inefficient
         for i in self.states:
@@ -37,31 +37,46 @@ class Q_agent():
         return tuple((s, v))
 
     def learn(self, iterations):
-        s = env.reset()
+        s = self.env.reset()
         s = self.state_discretizer(s[0], s[1])
         self.N = 0
         #print(s) 
         
+        
         for i in range(iterations):
-            env.render()
+            self.env.render()
             self.N += 1
             alpha = 1 / self.N
-            action = policy[s]
+            action = self.policy[s]
             #print(action)
-            ns, r, done, _ = env.env.step(action) #act and observe
+            ns, r, done, _ = self.env.env.step(action) #act and observe
             p = np.random.uniform()
             ns = self.state_discretizer(ns[0], ns[1]) #push the obs into an interval between states
             #print(ns) 
 
             if p > self.eps: #exploit
-                qsa_pairs[s][action] = (1 - alpha) * qsa_pairs[s][action] + alpha * (r + self.gamma * qsa_pairs[ns][np.argmax(qsa_pairs[s])]) 
-                policy[s] =  np.argmax(qsa_pairs[s]) #replacing action for best current action
+                self.qsa_pairs[s][action] = (1 - alpha) * self.qsa_pairs[s][action] + alpha * (r + self.gamma * self.qsa_pairs[ns][np.argmax(self.qsa_pairs[s])]) 
+                self.policy[s] =  np.argmax(self.qsa_pairs[s]) #replacing action for best current action
             else: #explore
-                qsa_pairs[s][action] = (1 - alpha) * qsa_pairs[s][action] + alpha * (r + self.gamma * qsa_pairs[ns][np.random.choice(3)]) #select random action
+                self.qsa_pairs[s][action] = (1 - alpha) * self.qsa_pairs[s][action] + alpha * (r + self.gamma * self.qsa_pairs[ns][np.random.choice(3)]) #select random action
 
+            self.total_reward += self.gamma ** self.N * r
+
+            if i % 100 == 0:
+                print("Iteration number {}, current reward: {}".format(i,self.total_reward))
             s = ns
         print(alpha)
-        env.close()
+        env.close() 
+
+
+        def run_episode(self, env, policy):
+            """
+            Run episode with learned policy
+            """
+            s = self.env.reset()
+            s = self.state_discretizer(s[0], s[1])
+            while not done:
+                ns, r, done, _ = env.env.step()
 
 
 env = gym.make("MountainCar-v0")
@@ -71,11 +86,17 @@ positions = np.linspace(env.env.min_position, env.env.max_position, 8) #discreti
 states = list(product(positions, [0,1])) #tuples: (discretised positions, neg or positive vel)
 qsa_pairs = {i:[0 for i in range(3)] for i in states}
 
+#another alpha (recall that it has to tend to 0 in the limit):
+
+
 policy = {i:np.random.choice(3) for i in states} #random initial policy     
 MrQ = Q_agent(env, gamma, states, qsa_pairs, policy, eps=1/3)
 
 for i in range(50):
-    MrQ.learn(300)
+    MrQ.learn(500)
+
+
+run_episode(env, policy)
 
 
 print(policy)
